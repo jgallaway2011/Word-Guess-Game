0b03977feaed02cff9293fec4543584a43ebbcfa
jgallaway2011 <jgallaway2011@gmail.com>
2018-06-23 00:31:27 -0500

    Updating HTML & JS files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -39,6 +39,9 @@ var hipsterWord = hipsterWords[Math.floor(Math.random() * hipsterWords.length)];
 // Start the game with zero wins
 var wins = 0;
 
+// Start the game with zero losses
+var losses = 0;
+
 // Computer starts user with a certain amount of guesses based on the length of the hipsterWord randomly selected
 var guessesRemaining = Math.ceil(hipsterWord.length * 8/3);
 
@@ -48,57 +51,43 @@ var hipsterWordHidden = [];
 // Empty array that will be used later to collect letters guessed by player
 var lettersGuessed = [];
 
+var hipsterWordDisplay = document.getElementById("currentWord");
+
 //====================================================================================================================
 
 // FUNCTIONS
 
 // Function to insert random word into game
 function insertHipseterWord() {
-    for ( var i = 0; i < hipsterWord.length; i++) {
-        hipsterWordHidden.push(" _ ");
+    for (var i = 0; i < hipsterWord.length; i++) {                                                            
+        hipsterWordHidden.push("  _  ");                                                                       
     }
-    document.getElementById("currentWord").innerHTML = hipsterWordHidden.join("  ");
+    hipsterWordDisplay.innerHTML = hipsterWordHidden.join("  ");                
 }
 
 // Function to display wins
-function winsTracker() {
-    document.querySelector("#wins").innerHTML = "Wins: " + wins;
+function winsTracker() {                                                                                       
+    document.querySelector("#wins").innerHTML = "Wins: " + wins;                                               
+  }
+
+// Function to display losses
+function lossesTracker() {                                                                                       
+    document.querySelector("#losses").innerHTML = "Losses: " + losses;                                               
   }
 
 // Function to show guesses remaining in current game
-function guessesRemainingTracker() {
+function guessesRemainingTracker() {                                                                           
     document.querySelector("#guessesRemaining").innerHTML = "Number of Guesses Remaining: " + guessesRemaining;
   }
-
-function lettersGuessedTracker() {
-    document.onkeyup = function(event) {
-        var letter = event.key.toUpperCase();
-
-        for (var i= 0; i < hipsterWord.length; i++) {
-            if (hipsterWord.charAt(i) === letter) {
-                hipsterWordHidden[i] = letter;
-                document.getElementById("currentWord").innerHTML = hipsterWordHidden.join("  ");
-            }
-        }
-
-        if (lettersGuessed.indexOf(letter) === -1) {                                                                          // Check if letter is in lettersGuessed Array
-            lettersGuessed.push(letter);                                                                                      // Insert letter in array if not guessed before
-            document.getElementById("lettersGuessed").innerHTML = "Letters Already Guessed: " + lettersGuessed.join("  "); 
-            guessesRemaining--;                                                                                               // Subtract guuesesRemaining by one
-            guessesRemainingTracker();                                                                                        // Reprint to display gueses remaining
-        } else {
-            alert("Letter already guessed!");                                                                                 //Alert user if letter guessed more than once
-        }
-    }
-}
-
-
 
 // MAIN PROCESS
 // ==============================================================================
 
-// Displays intial wins of zero
+// Displays initial wins of zero
 winsTracker();
+
+// Displays initial losses of zero
+lossesTracker();
 
 // Displays computer generated amount of guesses to start based on word length
 guessesRemainingTracker();
@@ -106,19 +95,51 @@ guessesRemainingTracker();
 // Displays word hidden on the document for the start of the game
 insertHipseterWord();
 
-if (hipsterWordHidden.indexOf(" _ ") !== -1) {
+document.onkeyup = function(event) {
+    var letter = event.key.toUpperCase();
 
-    //Adds any letters already guessed to the document 
-      lettersGuessedTracker();
+    for (var i= 0; i < hipsterWord.length; i++) {
+        if (hipsterWord.charAt(i) === letter) {
+            hipsterWordHidden[i] = letter;
+            document.getElementById("currentWord").innerHTML = hipsterWordHidden.join("  ");
+        }
+    }
 
-} else if (guessesRemaining >= 0 && hipsterWordHidden.indexOf(" _ ") == -1) {
+    if (lettersGuessed.indexOf(letter) === -1) {
+        lettersGuessed.push(letter);
+        document.getElementById("lettersGuessed").innerHTML = "Letters Already Guessed: " + lettersGuessed.join("  ");
+        guessesRemaining--;
+        guessesRemainingTracker();
+    } else {
+        alert("Letter already guessed!");
+    }
+    
+    if (guessesRemaining >= 0 && hipsterWordHidden.indexOf("  _  ") === -1) {
+        document.getElementById("currentWord").innerHTML = "You won!";
+        wins++;
+        winsTracker();
+        hipsterWord = hipsterWords[Math.floor(Math.random() * hipsterWords.length)];
+        insertHipseterWord();
+        guessesRemaining = Math.ceil(hipsterWord.length * 8/3);
+        guessesRemainingTracker();
+        lettersGuessed = [];
+        document.getElementById("lettersGuessed").innerHTML = "Letters Already Guessed: " + lettersGuessed.join("  ");
+        
 
-    document.getElementById("currentWord").innerHTML = "You won!";
-    wins++;
-    winsTracker();
+    } else if (guessesRemaining === 0 && hipsterWordHidden.indexOf("  _  ") !== -1) {
+        document.getElementById("currentWord").innerHTML = "You lost!";
+        losses++;
+        lossesTracker();
+        hipsterWord = hipsterWords[Math.floor(Math.random() * hipsterWords.length)];
+        insertHipseterWord();
+        guessesRemaining = Math.ceil(hipsterWord.length * 8/3);
+        guessesRemainingTracker();
+        lettersGuessed = [];
+        document.getElementById("lettersGuessed").innerHTML = "Letters Already Guessed: " + lettersGuessed.join("  ");
 
-} else {
+    } else {
+       // Keep Guessing Letters
+    }
 
-    document.getElementById("currentWord").innerHTML = "You lost! Try Again!";
-    insertHipseterWord();
 }
+
